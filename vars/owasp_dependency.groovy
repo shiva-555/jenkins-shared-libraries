1a6d7b28c9f23d652a122aaa2de1a73970dd4eb4
shiva-555 <91751287+shiva-555@users.noreply.github.com>
2026-06-22 20:42:18 +0530

    Add OWASP Dependency Check pipeline function

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,1 +1,15 @@
+def call(String scanPath='.') {
 
+    dependencyCheck(
+        odcInstallation: 'OWASP-Dependency-Check',
+        additionalArguments: """
+            --scan ${scanPath}
+            --format ALL
+            --prettyPrint
+        """
+    )
+
+    dependencyCheckPublisher(
+        pattern: '**/dependency-check-report.xml'
+    )
+}
